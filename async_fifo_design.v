5f676502f9ffc4291ced14c96feae0175f32a21e
Shivam007018 <iota66445@gmail.com>
2026-03-23 15:41:01 +0530

    Update async_fifo_design.v

--- async_fifo_design.v
+++ async_fifo_design.v
@@ -137,14 +137,14 @@ module FIFO #(parameter DSIZE = 8,
     wire [ASIZE-1:0] waddr, raddr;
     wire [ASIZE:0] wptr, rptr, wq2_rptr, rq2_wptr;
 
-    two_ff_sync #(ASIZE+1) sync_r2w (       // Read pointer syncronization to write clock domain
+    two_ff_sync #(ASIZE) sync_r2w (       // Read pointer syncronization to write clock domain
         .q2(wq2_rptr), 
         .din(rptr),
         .clk(wclk), 
         .rst_n(wrst_n)
     );
 
-    two_ff_sync #(ASIZE+1) sync_w2r (       // Write pointer syncronization to read clock domain
+    two_ff_sync #(ASIZE) sync_w2r (       // Write pointer syncronization to read clock domain
         .q2(rq2_wptr), 
         .din(wptr),
         .clk(rclk), 
